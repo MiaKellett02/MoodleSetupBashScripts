4fb17b8b86451425827aad590303fc8a08460b3c
mia <mia@debian.debian>
2026-07-02 14:57:35 +0100

    Php code sniffer stuff added

--- InstallPhpCodeSniffer.sh
+++ InstallPhpCodeSniffer.sh
@@ -1,2 +1,14 @@
+sudo apt update
+sudo apt upgrade
+
+# Download using curl
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+
+# Then test the downloaded PHARs
+php phpcs.phar -h
+php phpcbf.phar -h
+
+# Add moodle specific
 composer global config minimum-stability dev
 composer global require moodlehq/moodle-cs
